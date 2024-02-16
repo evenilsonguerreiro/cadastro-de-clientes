ef5ddc98bd33a011769b6851a65663e406f0f174
evenilson <evenilsonguerreiro@gmail.com>
2024-02-16 12:34:50 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,35 @@
+const inputNome = document.querySelector('#input-Nome')
+const inputEmail = document.querySelector('#input-Email')
+const inputTel = document.querySelector('#input-Tel')
+const form = document.querySelector('#form')
+
+
+let linhas = ''
+
+
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+
+
+    linha = `<tr>`
+    linha += `<td>${inputNome.value}</td>`
+    linha += `<td>${inputEmail.value}</td>`
+    linha += `<td>${inputTel.value}</td>`
+    linha += `</tr>`;
+    linhas += linha
+
+    const tbody = document.querySelector('#t-body')
+    
+    tbody.innerHTML = linhas
+
+
+    inputNome.value = ''
+    inputEmail.value = ''
+    inputTel.value = ''
+
+
+
+
+})
+
+
